2723bdcfaf4936c191d9c293de932855c937600f
AliHashempour <ali.hashempour@gmail.com>
2024-06-05 23:36:22 -0700

    feat: implement load & store

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -1,10 +1,12 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-memory/handler"
 	"go-memory/internal/repository"
 	"go-memory/internal/storage"
+	"os"
 	"strings"
 )
 
@@ -36,9 +38,13 @@ func (cp *CommandProcessor) ProcessCommand(input string) string {
 	case "keys":
 		return cp.processKeys(parts)
 	case "list":
-		return cp.CommandHandler.ListDatabasesCommand(cp.Store)
+		return cp.processList()
 	case "use":
 		return cp.processUse(parts)
+	case "dump":
+		return cp.DumpDatabase(parts)
+	case "load":
+		return cp.LoadDatabase(parts)
 	case "exit":
 		return "Exiting..."
 	default:
@@ -74,6 +80,10 @@ func (cp *CommandProcessor) processKeys(parts []string) string {
 	return cp.CommandHandler.KeysCommand(parts[1])
 }
 
+func (cp *CommandProcessor) processList() string {
+	return cp.CommandHandler.ListDatabasesCommand(cp.Store)
+}
+
 func (cp *CommandProcessor) processUse(parts []string) string {
 	if len(parts) < 2 {
 		return "Usage: use <dbName>"
@@ -85,4 +95,49 @@ func (cp *CommandProcessor) processUse(parts []string) string {
 	}
 	cp.CommandHandler = handler.NewCommandHandler(repository.NewMemoryRepository(cp.Store.CurrentDatabase))
 	return fmt.Sprintf("Switched to database: %s", dbName)
+}
+
+func (cp *CommandProcessor) DumpDatabase(parts []string) string {
+	if len(parts) != 3 {
+		return "Usage: dump <db_name> <path>"
+	}
+	dbName := parts[1]
+	filepath := parts[2]
+
+	db, exists := cp.Store.CheckDBExists(dbName)
+	if !exists {
+		return fmt.Sprintf("Database %s does not exist.", dbName)
+	}
+	data, err := json.Marshal(db.Data)
+	if err != nil {
+		return fmt.Sprintf("Failed to serialize database data: %s", err)
+	}
+	// Write data to file
+	err = os.WriteFile(filepath, data, 0644)
+	if err != nil {
+		return fmt.Sprintf("Failed to write to file: %s", err)
+	}
+	return "Database dumped successfully."
+}
+
+func (cp *CommandProcessor) LoadDatabase(parts []string) string {
+	if len(parts) != 3 {
+		return "Usage: load <path> <db_name>"
+	}
+	filepath := parts[1]
+	dbName := parts[2]
+
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return fmt.Sprintf("Failed to read file: %s", err)
+	}
+	var deserializedData map[string]interface{}
+	err = json.Unmarshal(data, &deserializedData)
+	if err != nil {
+		return fmt.Sprintf("Failed to deserialize data: %s", err)
+	}
+	db, _ := cp.Store.UseDatabase(dbName)
+	db.Data = deserializedData
+
+	return fmt.Sprintf("Database %s loaded successfully.", dbName)
 }
